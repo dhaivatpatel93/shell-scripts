b87ced0d529f9b578911a46ec75d8aec8b7a6d09
root <root@localhost.localdomain>
2018-02-26 22:23:14 +0530

    Second commit

---
+++ setvars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#set variables and assign values to them
+MYUSERNAME=dhaivat
+MYPASSWORD=patel
+STARTOFSCRIPT=line1
+ENDOFSCRIPT=line6
+#display two vars to terminal, in this case, MYUSERNAME and MYPASSWORD
+#use echo command to display variables
+echo "MYUSERNAME: $MYUSERNAME"
+echo "MYPASSWORD: $MYPASSWORD"
